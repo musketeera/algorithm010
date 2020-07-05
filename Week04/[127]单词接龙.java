1ea5547603822846376a9b25cd47f4fe43ddf3f4
musketeer <jrlcc@hrbeu.edu.cn>
2020-07-05 11:41:15 +0800

    question solution

---
+++ Week04/[127]单词接龙.java
@@ -0,0 +1,110 @@
+//给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+// 
+//
+// 
+// 每次转换只能改变一个字母。 
+// 转换过程中的中间单词必须是字典中的单词。 
+// 
+//
+// 说明: 
+//
+// 
+// 如果不存在这样的转换序列，返回 0。 
+// 所有单词具有相同的长度。 
+// 所有单词只由小写字母组成。 
+// 字典中不存在重复的单词。 
+// 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。 
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//beginWord = "hit",
+//endWord = "cog",
+//wordList = ["hot","dot","dog","lot","log","cog"]
+//
+//输出: 5
+//
+//解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+//     返回它的长度 5。
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//beginWord = "hit"
+//endWord = "cog"
+//wordList = ["hot","dot","dog","lot","log"]
+//
+//输出: 0
+//
+//解释: endWord "cog" 不在字典中，所以无法进行转换。 
+// Related Topics 广度优先搜索 
+// 👍 364 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+广度优先遍历，终止条件就是找到结束单词。
+时间复杂度O(M×N)
+空间复杂度O(M×N)
+ */
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        // Since all words are of same length.
+        int L = beginWord.length();
+
+        // Dictionary to hold combination of words that can be formed,
+        // from any given word. By changing one letter at a time.
+        Map<String, List<String>> allComboDict = new HashMap<>();
+
+        wordList.forEach(
+                word -> {
+                    for (int i = 0; i < L; i++) {
+                        // Key is the generic word
+                        // Value is a list of words which have the same intermediate generic word.
+                        String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+                        List<String> transformations = allComboDict.getOrDefault(newWord, new ArrayList<>());
+                        transformations.add(word);
+                        allComboDict.put(newWord, transformations);
+                    }
+                });
+
+        // Queue for BFS
+        Queue<Pair<String, Integer>> Q = new LinkedList<>();
+        Q.add(new Pair(beginWord, 1));
+
+        // Visited to make sure we don't repeat processing same word.
+        Map<String, Boolean> visited = new HashMap<>();
+        visited.put(beginWord, true);
+
+        while (!Q.isEmpty()) {
+            Pair<String, Integer> node = Q.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+            for (int i = 0; i < L; i++) {
+
+                // Intermediate words for current word
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+
+                // Next states are all the words which share the same intermediate state.
+                for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+                    // If at any point if we find what we are looking for
+                    // i.e. the end word - we can return with the answer.
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    // Otherwise, add it to the BFS Queue. Also mark it visited
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        Q.add(new Pair(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ Week04/[200]岛屿数量.java
@@ -0,0 +1,122 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//[
+//['1','1','1','1','0'],
+//['1','1','0','1','0'],
+//['1','1','0','0','0'],
+//['0','0','0','0','0']
+//]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//[
+//['1','1','0','0','0'],
+//['1','1','0','0','0'],
+//['0','0','1','0','0'],
+//['0','0','0','1','1']
+//]
+//输出: 3
+//解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 
+// 👍 636 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+使用并查集
+ */
+class Solution {
+    class UnionFind {
+        int count;
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) {
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) {
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx;
+                    rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        UnionFind uf = new UnionFind(grid);
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r-1][c] == '1') {
+                        uf.union(r * nc + c, (r-1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r+1][c] == '1') {
+                        uf.union(r * nc + c, (r+1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c-1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c+1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
+                }
+            }
+        }
+
+        return uf.getCount();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
